72baac70d1d71bd470f205e13344b55060b88f28
Sam Burcksen <sam.burcksen@gmail.com>
2025-01-03 20:37:38 +0100

    Adjust channel of neovim to remove impurity

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -126,22 +126,11 @@
     })
 
     brightnessctl
-
-    unstable.kanata
-    unstable.neovim
+    kanata
   ];
 
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   programs.neovim = {
-  #  enable = true;
+    enable = true;
     defaultEditor = true;
   };
   programs.hyprland.enable = true;
@@ -171,9 +160,6 @@
     pkgs.nerd-fonts.jetbrains-mono
   ];
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
   # Audio
   security.rtkit.enable = true;
   services.pipewire = {
@@ -189,18 +175,5 @@
   hardware.bluetooth.powerOnBoot = true;
   services.blueman.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
+  system.stateVersion = "24.05";
 }
